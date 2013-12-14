157539d1773bdae66ff156112e993bffb871e1dd
sanemat <o.gata.ken@gmail.com>
2013-12-15 01:53:28 +0900

    Add awesome_scrub logic

--- lib/awesome_scrub.rb
+++ lib/awesome_scrub.rb
@@ -1,5 +1,14 @@
 require "awesome_scrub/version"
 
 module AwesomeScrub
-  # Your code goes here...
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+  end
+
+  def awesome_scrub(args)
+    puts 'awesome-awesome-awesome'
+  end
 end
