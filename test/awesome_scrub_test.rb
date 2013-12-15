58b7bf8b90d321f52fef9af99d0c1d248aec8bef
sanemat <o.gata.ken@gmail.com>
2013-12-15 21:08:26 +0900

    Add test skelton

--- test/awesome_scrub_test.rb
+++ test/awesome_scrub_test.rb
@@ -1,7 +1,25 @@
 require 'minitest/autorun'
+require_relative '../lib/awesome_scrub'
 
-class TestScrub < Minitest::Test
-  def test_foo
-    fail
+module AwesomeScrub
+  class AwesomeScrubTest < Minitest::Test
+
+    class TestClassWithInstanceAwesomeScrubs
+      include AwesomeScrub
+    end
+
+    class TestClassWithClassAwesomeScrubs
+      extend AwesomeScrub
+    end
+
+    def setup
+      @instance_with_modules = TestClassWithInstanceAwesomeScrubs.new
+    end
+
+    def test_awesome_scrub
+      [@instance_with_modules, TestClassWithClassAwesomeScrubs].each do |awesome_scrub|
+        assert_equal('foo', awesome_scrub.awesome_scrub('foo'))
+      end
+    end
   end
 end
