76bfd6b18a546751f829c3cdad2b8eb55a5b827a
sanemat <o.gata.ken@gmail.com>
2013-12-17 16:07:53 +0900

    call scrub

--- test/awesome_scrub_test.rb
+++ test/awesome_scrub_test.rb
@@ -21,5 +21,14 @@ module AwesomeScrub
         assert_equal('foo', awesome_scrub.awesome_scrub('foo'))
       end
     end
+
+    def test_call_scrub
+      [@instance_with_modules, TestClassWithClassAwesomeScrubs, AwesomeScrub].each do |awesome_scrub|
+        mock = MiniTest::Mock.new
+        mock.expect(:scrub, 'woot')
+        awesome_scrub.awesome_scrub(mock)
+        assert mock.verify
+      end
+    end
   end
 end
